e0c4ece6579a1e1f76d05b8c465de7e9bb024272
Inatan <inatan_hertzog@hotmail.com>
2021-05-08 23:56:21 -0300

    Starting tests

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,10 +1,44 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @option ["users", "foods"]
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn list, report -> sum_value(list, report) end)
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
+  def fetch_higher_cost(report, option) when option in @option do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option) do
+    {:error, "Invalid option"}
+  end
+
+  defp sum_value([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+
+    %{"users" => users, "foods" => foods}
+  end
 end
--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -1,8 +1,12 @@
 defmodule ReportsGeneratorTest do
   use ExUnit.Case
-  doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  describe "build/1" do
+    test "greets the world" do
+      assert ReportsGenerator.hello() == :world
+    end
+  end
+
+  describe "fetch_higher_cost/2" do
   end
 end
